7c0e53d8188cb98bbad8a9235da38e73a5d83129
Rene Brglez <rene.brglez@gmail.com>
2025-11-08 17:31:11 +0100

    WIP: stream-segmenter: updated/simplified

--- modules/stream-segmenter/rtl/stream_segmenter.vhd
+++ modules/stream-segmenter/rtl/stream_segmenter.vhd
@@ -17,8 +17,8 @@ use olo.olo_base_pkg_math.all;
 --------------------------------------------------------------------------------
 entity stream_segmenter is
     generic (
-        G_STREAM_WIDTH     : positive;
-        G_MAX_PACKET_BYTES : positive := 255
+        G_STREAM_WIDTH         : positive;
+        G_MAX_WORDS_PER_PACKET : positive := 255
     );
     port (
         i_clk : in std_logic;
@@ -27,8 +27,8 @@ entity stream_segmenter is
         ------------------------------------------------------------------------
         -- Static Configuration
         ------------------------------------------------------------------------
-        i_en           : in std_logic                                                       := '1';
-        i_packet_bytes : in std_logic_vector(log2ceil(G_MAX_PACKET_BYTES + 1) - 1 downto 0) := toUslv(G_MAX_PACKET_BYTES, log2ceil(G_MAX_PACKET_BYTES + 1));
+        i_en               : in std_logic                                                           := '1';
+        i_words_per_packet : in std_logic_vector(log2ceil(G_MAX_WORDS_PER_PACKET + 1) - 1 downto 0) := toUslv(G_MAX_WORDS_PER_PACKET, log2ceil(G_MAX_WORDS_PER_PACKET + 1));
 
         ------------------------------------------------------------------------
         -- In Stream Interface
@@ -36,7 +36,6 @@ entity stream_segmenter is
         i_in_stream_valid : in  std_logic;
         i_in_stream_last  : in  std_logic := '0';
         o_in_stream_ready : out std_logic;
-        i_in_stream_be    : in  std_logic_vector(G_STREAM_WIDTH / 8 - 1 downto 0) := (others => '1');
         i_in_stream_data  : in  std_logic_vector(G_STREAM_WIDTH - 1 downto 0);
 
         ------------------------------------------------------------------------
@@ -45,7 +44,6 @@ entity stream_segmenter is
         o_out_stream_valid : out std_logic;
         o_out_stream_last  : out std_logic;
         i_out_stream_ready : in  std_logic;
-        o_out_stream_be    : out std_logic_vector(G_STREAM_WIDTH / 8 - 1 downto 0);
         o_out_stream_data  : out std_logic_vector(G_STREAM_WIDTH - 1 downto 0)
     );
 end entity stream_segmenter;
@@ -55,37 +53,13 @@ architecture rtl of stream_segmenter is
     ----------------------------------------------------------------------------
     -- Constants
     ----------------------------------------------------------------------------
-    -- last + be + data
-    constant C_CONCAT_WIDTH : natural := 1 + G_STREAM_WIDTH / 8 + G_STREAM_WIDTH;
-
-    constant C_BYTES_PER_DATA_BEAT : natural := G_STREAM_WIDTH / 8;
+    constant C_WORDS_PER_PACKET_WIDTH : natural := log2ceil(G_MAX_WORDS_PER_PACKET + 1);
 
     ----------------------------------------------------------------------------
     -- Types
     ----------------------------------------------------------------------------
-    -- state type
-    type state_t is (
-            DATA_S,
-            LAST_S,
-            DUPLICATE_S
-        );
-
     type reg_t is record
-        byte_cnt : natural range 0 to G_MAX_PACKET_BYTES + 1;
-        
-        in_pl_valid       : std_logic;
-        in_pl_data        : std_logic_vector(G_STREAM_WIDTH - 1 downto 0);
-        in_pl_last        : std_logic;
-        in_pl_be          : std_logic_vector(G_STREAM_WIDTH / 8 - 1 downto 0);
-        in_pl_concat_data : std_logic_vector(C_CONCAT_WIDTH - 1 downto 0);
-
-        in_pl_ready : std_logic;
-
-        data_beat_duplicate : std_logic;
-        data_duplicate      : std_logic_vector(G_STREAM_WIDTH - 1 downto 0);
-
-        --
-        state : state_t;
+        word_cnt : natural range 0 to C_WORDS_PER_PACKET_WIDTH;
     end record;
 
     ----------------------------------------------------------------------------
@@ -97,20 +71,22 @@ architecture rtl of stream_segmenter is
     ----------------------------------------------------------------------------
     -- Instantiation signals
     ----------------------------------------------------------------------------
+    signal in_pl_valid       : std_logic;
     signal in_pl_ready       : std_logic;
-    signal in_pl_concat_data : std_logic_vector(C_CONCAT_WIDTH - 1 downto 0);
+    signal in_pl_last        : std_logic;
+    signal in_pl_concat_data : std_logic_vector(G_STREAM_WIDTH downto 0);
 
-    signal out_pl_concat_data : std_logic_vector(C_CONCAT_WIDTH - 1 downto 0);
+    signal out_pl_concat_data : std_logic_vector(G_STREAM_WIDTH downto 0);
 
 begin
 
-    in_pl_concat_data <= r.in_pl_last & r.in_pl_be & r.in_pl_data;
-    o_in_stream_ready <= r.in_pl_ready;
+    in_pl_concat_data <= in_pl_last & i_in_stream_data;
+    o_in_stream_ready <= in_pl_ready;
 
     -- break logic chain between in ready and out ready
     u_pl_stage : entity olo.olo_base_pl_stage
         generic map (
-            Width_g    => C_CONCAT_WIDTH,
+            Width_g    => G_STREAM_WIDTH + 1,
             UseReady_g => true,
             Stages_g   => 1
         )
@@ -118,7 +94,7 @@ begin
             Clk => i_clk,
             Rst => i_rst,
 
-            In_Valid => r.in_pl_valid,
+            In_Valid => in_pl_valid,
             In_Ready => in_pl_ready,
             In_Data  => in_pl_concat_data,
 
@@ -127,115 +103,45 @@ begin
             Out_Data  => out_pl_concat_data
         );
 
-    o_out_stream_last <= out_pl_concat_data(C_CONCAT_WIDTH - 1);
-    o_out_stream_be   <= out_pl_concat_data((C_CONCAT_WIDTH - 1) - 1 downto G_STREAM_WIDTH);
+    o_out_stream_last <= out_pl_concat_data(G_STREAM_WIDTH);
     o_out_stream_data <= out_pl_concat_data(G_STREAM_WIDTH - 1 downto 0);
 
     ----------------------------------------------------------------------------
     -- Combinatorial process
     ----------------------------------------------------------------------------
     p_comb : process(all)
-        variable v              : reg_t;
-        variable packet_bytes_v : unsigned(i_packet_bytes'range);
-        variable byte_rem_v     : integer;
+        variable v : reg_t;
     begin
         -- Hold variables stable
         v := r;
 
-        packet_bytes_v := unsigned(i_packet_bytes);
-        byte_rem_v     := to_integer(packet_bytes_v) - C_BYTES_PER_DATA_BEAT - r.byte_cnt;
-
         if (i_en = '1') then
-            v.in_pl_valid := i_in_stream_valid;
+            in_pl_valid <= i_in_stream_valid;
         else
-            v.in_pl_valid := '0';
+            in_pl_valid <= '0';
+        end if;
+        in_pl_last <= i_in_stream_last;
+
+        ------------------------------------------------------------------------
+        ------------------------------------------------------------------------
+
+        if (in_pl_valid = '1' and in_pl_ready = '1') then
+            v.word_cnt := r.word_cnt + 1;
+
+            if (in_pl_last = '1') then
+                v.word_cnt := 0;
+            end if;
+
+
+            if (r.word_cnt > unsigned(i_words_per_packet) - 2) then
+                in_pl_last <= '1';
+                v.word_cnt := 0;
+            end if;
         end if;
 
+
         ------------------------------------------------------------------------
-        -- FSM
         ------------------------------------------------------------------------
-        case (r.state) is
-            --------------------------------------------------------------------
-            when DATA_S =>
-
-                v.in_pl_data := i_in_stream_data;
-                v.in_pl_last := i_in_stream_last;
-                v.in_pl_be   := i_in_stream_be;
-
-                v.in_pl_ready := '1';
-
-
-                if (r.in_pl_valid = '1' and in_pl_ready = '1') then
-
-                    v.byte_cnt := r.byte_cnt + count(r.in_pl_be, '1');
-
-                    if (r.byte_cnt >= packet_bytes_v - 2*C_BYTES_PER_DATA_BEAT) then
-
-                        v.in_pl_last := '1';
-
-                        v.data_beat_duplicate := '0';
-
-                        if (byte_rem_v /= C_BYTES_PER_DATA_BEAT) then
-                            v.in_pl_be                                                              := (others => '0');
-                            v.in_pl_be(r.in_pl_be'length - 1 downto r.in_pl_be'length - byte_rem_v) := (others => '1');
-                            v.data_beat_duplicate                                                   := '1';
-                            v.in_pl_ready                                                           := '0';
-
-                            v.byte_cnt := C_BYTES_PER_DATA_BEAT - byte_rem_v;
-                        end if;
-
-                        v.state := LAST_S;
-                    end if;
-                end if;
-
-            --------------------------------------------------------------------
-            when LAST_S =>
-                v.in_pl_data := i_in_stream_data;
-                v.in_pl_last := i_in_stream_last;
-                v.in_pl_be   := i_in_stream_be;
-
-                if (r.in_pl_valid = '1' and in_pl_ready = '1') then
-                    v.in_pl_last := '0';
-
-                    v.in_pl_ready := '1';
-
-                    if (r.data_beat_duplicate = '1') then
-                        v.data_duplicate := r.in_pl_data;
-
-                        v.in_pl_data := r.in_pl_data;
-                        v.in_pl_be   := not r.in_pl_be;
-
-                        v.state := DUPLICATE_S;
-
-                    else
-
-                        v.byte_cnt := 0;
-                        v.state    := DATA_S;
-
-                    end if;
-                end if;
-
-            --------------------------------------------------------------------
-            when DUPLICATE_S =>
-
-                v.in_pl_data := r.data_duplicate;
-
-                if (r.in_pl_valid = '1' and in_pl_ready = '1') then
-                    v.in_pl_ready := '1';
-
-                    v.in_pl_data := i_in_stream_data;
-                    v.in_pl_last := i_in_stream_last;
-                    v.in_pl_be   := i_in_stream_be;
-
-                    v.state := DATA_S;
-                end if;
-
-
-            --------------------------------------------------------------------
-            when others =>
-                null;
-        ------------------------------------------------------------------------
-        end case;
 
         -- Apply to record
         r_next <= v;
@@ -250,13 +156,8 @@ begin
         if rising_edge(i_clk) then
             r <= r_next;
             if (i_rst = '1') then
-                r.byte_cnt <= 0;
+                r.word_cnt <= 0;
 
-                r.in_pl_valid <= '0';
-
-                r.data_beat_duplicate <= '0';
-                --
-                r.state <= DATA_S;
             end if;
         end if;
     end process;
--- modules/stream-segmenter/tb/stream_segmenter_tb.vhd
+++ modules/stream-segmenter/tb/stream_segmenter_tb.vhd
@@ -15,8 +15,8 @@ use olo.olo_base_pkg_math.all;
 
 entity stream_segmenter_tb is
     generic(
-        G_STREAM_WIDTH     : positive := 8 * 3;
-        G_MAX_PACKET_BYTES : positive := 15
+        G_STREAM_WIDTH         : positive := 8 * 3;
+        G_MAX_WORDS_PER_PACKET : positive := 20
     );
 end entity stream_segmenter_tb;
 
@@ -30,18 +30,17 @@ architecture tb of stream_segmenter_tb is
     signal i_clk : std_logic;
     signal i_rst : std_logic;
 
-    signal i_packet_bytes : std_logic_vector(log2ceil(G_MAX_PACKET_BYTES + 1) - 1 downto 0);
+    signal i_en               : std_logic;
+    signal i_words_per_packet : std_logic_vector(log2ceil(G_MAX_WORDS_PER_PACKET + 1) - 1 downto 0);
 
-    signal in_stream_valid  : std_logic;
-    signal in_stream_ready  : std_logic;
-    signal i_in_stream_be   : std_logic_vector(G_STREAM_WIDTH / 8 - 1 downto 0) := (others => '1');
-    signal i_in_stream_last : std_logic                                         := '0';
-    signal in_stream_data   : std_logic_vector(G_STREAM_WIDTH - 1 downto 0);
+    signal in_stream_valid : std_logic;
+    signal in_stream_ready : std_logic;
+    signal in_stream_last  : std_logic;
+    signal in_stream_data  : std_logic_vector(G_STREAM_WIDTH - 1 downto 0);
 
     signal o_out_stream_valid : std_logic;
     signal o_out_stream_last  : std_logic;
     signal i_out_stream_ready : std_logic;
-    signal o_out_stream_be    : std_logic_vector(G_STREAM_WIDTH / 8 - 1 downto 0);
     signal o_out_stream_data  : std_logic_vector(G_STREAM_WIDTH - 1 downto 0);
 
     ----------------------------------------------------------------------------
@@ -49,13 +48,17 @@ architecture tb of stream_segmenter_tb is
     ----------------------------------------------------------------------------
     signal prbs_en : std_logic;
 
-    signal out_prbs_valid : std_logic;
-    signal out_prbs_ready : std_logic;
-    signal out_prbs_data  : std_logic_vector(G_STREAM_WIDTH - 1 downto 0);
+    signal prbs_valid : std_logic;
+    signal prbs_ready : std_logic;
+    signal prbs_data  : std_logic_vector(G_STREAM_WIDTH - 1 downto 0);
 
-    signal stall      : std_logic;
-    signal prbs_stall : std_logic;
-    signal stall_en   : std_logic;
+    signal stall         : std_logic;
+    signal prbs_stall    : std_logic;
+    signal prbs_stall_en : std_logic;
+
+    signal prbs_last    : std_logic;
+    signal prbs_last_en : std_logic;
+
 begin
 
     ----------------------------------------------------------------------------
@@ -63,68 +66,83 @@ begin
     ----------------------------------------------------------------------------
     dut : entity work.stream_segmenter
         generic map (
-            G_STREAM_WIDTH     => G_STREAM_WIDTH,
-            G_MAX_PACKET_BYTES => G_MAX_PACKET_BYTES
+            G_STREAM_WIDTH         => G_STREAM_WIDTH,
+            G_MAX_WORDS_PER_PACKET => G_MAX_WORDS_PER_PACKET
         )
         port map (
             i_clk => i_clk,
             i_rst => i_rst,
 
-            i_packet_bytes => i_packet_bytes,
+            i_words_per_packet => i_words_per_packet,
 
             i_in_stream_valid => in_stream_valid,
-            i_in_stream_last  => i_in_stream_last,
+            i_in_stream_last  => in_stream_last,
             o_in_stream_ready => in_stream_ready,
-            i_in_stream_be    => i_in_stream_be,
             i_in_stream_data  => in_stream_data,
 
             o_out_stream_valid => o_out_stream_valid,
             o_out_stream_last  => o_out_stream_last,
             i_out_stream_ready => i_out_stream_ready,
-            o_out_stream_be    => o_out_stream_be,
             o_out_stream_data  => o_out_stream_data
         );
 
 
-    out_prbs_ready  <= in_stream_ready and prbs_en and not(stall);
-    in_stream_valid <= out_prbs_valid and prbs_en and not(stall);
-    in_stream_data  <= out_prbs_data;
+    prbs_ready      <= in_stream_ready and prbs_en and not(stall);
+    in_stream_valid <= prbs_valid and prbs_en and not(stall);
 
-    u_prbs : entity olo.olo_base_prbs
-        generic map (
-            Polynomial_g    => Polynomial_Prbs16_c,
-            Seed_g          => x"1234",
-            BitsPerSymbol_g => G_STREAM_WIDTH
-        )
-        port map (
-            Clk => i_clk,
-            Rst => i_rst,
+    in_stream_last <= prbs_last when prbs_last_en = '1' else '0';
 
-            Out_Data  => out_prbs_data,
-            Out_Ready => out_prbs_ready,
-            Out_Valid => out_prbs_valid
-        );
+        in_stream_data <= prbs_data;
 
-    u_stall_prbs : entity olo.olo_base_prbs
-        generic map (
-            Polynomial_g    => Polynomial_Prbs16_c,
-            Seed_g          => x"F12A",
-            BitsPerSymbol_g => 1
-        )
-        port map (
-            Clk => i_clk,
-            Rst => i_rst,
+        u_prbs : entity olo.olo_base_prbs
+            generic map (
+                Polynomial_g    => Polynomial_Prbs16_c,
+                Seed_g          => x"1234",
+                BitsPerSymbol_g => G_STREAM_WIDTH
+            )
+            port map (
+                Clk => i_clk,
+                Rst => i_rst,
 
-            Out_Data(0) => prbs_stall,
-            Out_Ready   => stall_en
-        );
+                Out_Data  => prbs_data,
+                Out_Ready => prbs_ready,
+                Out_Valid => prbs_valid
+            );
 
-    stall <= prbs_stall when stall_en = '1' else '0';
+        u_stall_prbs : entity olo.olo_base_prbs
+            generic map (
+                Polynomial_g    => Polynomial_Prbs16_c,
+                Seed_g          => x"F12A",
+                BitsPerSymbol_g => 1
+            )
+            port map (
+                Clk => i_clk,
+                Rst => i_rst,
 
-    ----------------------------------------------------------------------------
-    -- Clock generation
-    ----------------------------------------------------------------------------
-    tb_clock(i_clk, C_CLK_PERIOD);
+                Out_Data(0) => prbs_stall,
+                Out_Ready   => prbs_stall_en
+            );
+
+        stall <= prbs_stall when prbs_stall_en = '1' else '0';
+
+
+        u_last_prbs : entity olo.olo_base_prbs
+            generic map (
+                Polynomial_g    => Polynomial_Prbs16_c,
+                Seed_g          => x"AB87",
+                BitsPerSymbol_g => 1
+            )
+            port map (
+                Clk => i_clk,
+                Rst => i_rst,
+
+                Out_Data(0) => prbs_last,
+                Out_Ready   => prbs_last_en
+            );
+        ----------------------------------------------------------------------------
+        -- Clock generation
+        ----------------------------------------------------------------------------
+        tb_clock(i_clk, C_CLK_PERIOD);
 
     ----------------------------------------------------------------------------
     -- Simulation process
@@ -138,14 +156,15 @@ begin
         ------------------------------------------------------------------------
         i_rst <= '0';
 
-        stall_en           <= '0';
-        i_packet_bytes     <= (others => '0');
+        i_en               <= '0';
+        prbs_stall_en      <= '0';
+        i_words_per_packet <= (others => '0');
         i_out_stream_ready <= '0';
 
-        prbs_en <= '0';
+        prbs_last_en <= '0';
 
-        i_packet_bytes <= toUslv(12, i_packet_bytes'length);
-        tb_clk_period(i_clk, 10);
+        prbs_en            <= '1';
+        i_out_stream_ready <= '1';
 
         ------------------------------------------------------------------------
         -- Reset
@@ -154,19 +173,21 @@ begin
         tb_clk_period(i_clk, 10);
         i_rst <= '0';
 
-        tb_clk_period(i_clk, 10);
-
         ------------------------------------------------------------------------
         -- NEW TEST
         ------------------------------------------------------------------------
-        i_out_stream_ready <= '1';
-        tb_clk_period(i_clk);
-        prbs_en <= '1';
-        tb_clk_period(i_clk, 15);
+
+        i_words_per_packet <= toUslv(3, i_words_per_packet'length);
+        tb_clk_period(i_clk, 100);
 
 
-        i_packet_bytes <= toUslv(11, i_packet_bytes'length);
-        tb_clk_period(i_clk, 20);
+        i_words_per_packet <= toUslv(11, i_words_per_packet'length);
+        tb_clk_period(i_clk, 100);
+
+
+        prbs_last_en <= '1';
+        i_words_per_packet <= toUslv(2, i_words_per_packet'length);
+        tb_clk_period(i_clk, 100);
 
         ------------------------------------------------------------------------
         -- OLD TEST
@@ -181,15 +202,15 @@ begin
         --        tb_clk_period(i_clk, 100);
         --
         --        for i in 0 to 8 - 1 loop
-        --            i_packet_bytes <= toUslv(i mod 4, G_PACKET_LENGTH_WIDTH);
+        --            i_words_per_packet <= toUslv(i mod 4, G_PACKET_LENGTH_WIDTH);
         --            tb_clk_period(i_clk, 100);
         --        end loop;
         --
         --
-        --        stall_en <= '1';
+        --        prbs_stall_en <= '1';
         --
         --        for i in 0 to 8 - 1 loop
-        --            i_packet_bytes <= toUslv(i mod 4, G_PACKET_LENGTH_WIDTH);
+        --            i_words_per_packet <= toUslv(i mod 4, G_PACKET_LENGTH_WIDTH);
         --            tb_clk_period(i_clk, 100);
         --        end loop;
         --
